aa9b9689c857c4e888cf63c5b3d2bfcfcd3cd711
Francesco Saccone <francesco@francescosaccone.com>
2025-03-29 15:04:54 +0100

    feat: add port argument
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hermes.c
+++ hermes.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void
 print_usage(char *program_name) {
-	printf("usage: %s", program_name);
+	printf("usage: %s [-p port]", program_name);
 }
 
 int
 main(int argc, char *argv[]) {
 	char *program_name = argv[0];
-	int i;
+	int i, port = 80;
 
 	for (i = 1; i < argc; i++) {
 		char *argument = argv[i];
+		int value_exists = i + 1 < argc;
 
 		if (argument[0] != '-') {
 			print_usage(program_name);
@@ -19,6 +21,14 @@ main(int argc, char *argv[]) {
 		}
 
 		switch (argument[1]) {
+		case 'p':
+			if (!value_exists) {
+				print_usage(program_name);
+				return 1;
+			}
+			port = atoi(argv[i + 1]);
+			i++;
+			break;
 		default:
 			print_usage(program_name);
 			return 1;
