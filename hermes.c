df5e821ad6a076e88efc6c880b8c21acb53260bb
Francesco Saccone <francesco@francescosaccone.com>
2025-03-29 14:55:45 +0100

    feat: add arguments parsing
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- hermes.c
+++ hermes.c
@@ -8,6 +8,22 @@ print_usage(char *program_name) {
 int
 main(int argc, char *argv[]) {
 	char *program_name = argv[0];
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		char *argument = argv[i];
+
+		if (argument[0] != '-') {
+			print_usage(program_name);
+			return 1;
+		}
+
+		switch (argument[1]) {
+		default:
+			print_usage(program_name);
+			return 1;
+		}
+	}
 
 	return 0;
 }
