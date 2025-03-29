fb5457d3792488b775fd5c1d4da6c031414081c2
Francesco Saccone <francesco@francescosaccone.com>
2025-03-29 19:38:09 +0100

    docs: add description comment to create_socket
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- socket.h
+++ socket.h
@@ -1,6 +1,8 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
+/* creates a socket and a local address at the given port, binds them,
+   listens for connections and returns the socket file descriptor */
 int
 create_socket(unsigned int port);
 
