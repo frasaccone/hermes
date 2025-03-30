a56df6a68aa105282f2e7590c3d817550e8123bd
Francesco Saccone <francesco@francescosaccone.com>
2025-03-30 20:15:13 +0200

    feat: add file_exists function
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- file.c
+++ file.c
@@ -1,1 +1,7 @@
 #include "file.h"
+
+int
+file_exists(const char *path) {
+	struct stat buffer;
+	return (stat(path, &buffer) == 0);
+}
--- file.h
+++ file.h
@@ -1,4 +1,7 @@
 #ifndef FILE_H
 #define FILE_H
 
+int
+file_exists(const char *path);
+
 #endif
