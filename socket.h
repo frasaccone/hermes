f2bf66dec9cf9b3c838bd1b7f4dd392511cdf696
Francesco Saccone <francesco@francescosaccone.com>
2025-03-31 14:57:40 +0200

    docs: add missing punctuation and paragraph breaks to comments
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- socket.h
+++ socket.h
@@ -3,27 +3,29 @@
 
 /*
  * Creates a socket and a local address at the given port, binds them,
- * listens for connections and returns the socket file descriptor
+ * listens for connections and returns the socket file descriptor.
  */
 int
 create_socket(unsigned int port);
 
 /*
- * Closes a socket
+ * Closes a socket.
  */
 void
 close_socket(int socket_fd);
 
 /* 
  * Accepts a connection from the server socket and returns the client socket
- * file descriptor
+ * file descriptor.
  */
 int
 accept_client(int server_socket_fd);
 
 /*
- * Writes the client request to a buffer, returns -1 in case of an error and 0
- * otherwise
+ * Writes the client request to a buffer.
+ *
+ * Returns -1 in case of an error and 0
+ * otherwise.
  */
 int
 read_client_request(int client_socket_fd,
