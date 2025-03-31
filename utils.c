a6ba79cd366fa8fd9208d9cb70b58d8be51f67dc
Francesco Saccone <francesco@francescosaccone.com>
2025-03-31 13:51:10 +0200

    feat: support additional arguments in 'critical' function
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- utils.c
+++ utils.c
@@ -1,10 +1,18 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "utils.h"
 
 void
-critical(const char *message) {
-	fprintf(stderr, "%s\n", message);
+critical(const char *message, ...) {
+	va_list arguments;
+
+	va_start(arguments, message);
+	vfprintf(stderr, message, arguments);
+	va_end(arguments);
+
+	fprintf(stderr, "\n");
+
 	exit(1);
 }
