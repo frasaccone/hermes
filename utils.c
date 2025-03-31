ce4ed14ebb52ec3241fff8efe18fb901b44d0b1e
Francesco Saccone <francesco@francescosaccone.com>
2025-03-31 13:43:16 +0200

    feat: add 'critical' function
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- utils.c
+++ utils.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "utils.h"
+
+void
+critical(const char *message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
--- utils.h
+++ utils.h
@@ -1,4 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+void
+critical(const char *message);
+
 #endif
