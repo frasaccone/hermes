2944a697ac4add003fb682eaa584964309cb45f3
Francesco Saccone <francesco@francescosaccone.com>
2025-04-01 10:44:23 +0200

    feat: remove trailing slash when normalising paths
    Signed-off-by: Francesco Saccone <francesco@francescosaccone.com>

--- file.c
+++ file.c
@@ -31,7 +31,8 @@ get_normalised_path(char *path) {
 		if (path[i] != '/' ||
 		    /* In the following conditions, we have path[i] == '/'. */
 		    i == 0 || /* Keep leading slash */
-		    path[i - 1] != '/'/* No continous slashes */) {
+		    path[i - 1] != '/' || /* No continous slashes */
+		    path[i + 1] != '\0' /* No trailing slash */) {
 			normalised[j] = path[i];
 			j++;
 		}
